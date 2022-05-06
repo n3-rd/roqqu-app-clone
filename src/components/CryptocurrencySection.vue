9a4b2627458b10791f84b9c0619a6ae98880db57
N3-rd <godwinjdayo@gmail.com>
2022-05-05 18:47:23 -0700

    used proxy

--- src/components/CryptocurrencySection.vue
+++ src/components/CryptocurrencySection.vue
@@ -40,7 +40,7 @@ export default {
   },
   methods: {
     getCoinsData() {
-      fetch(`https://api.coinranking.com/v2/coins`)
+      fetch(`https://n3rdy-cors-proxy.glitch.me/useproxy?link=https://api.coinranking.com/v2/coins`)
         .then((response) => response.json())
         .then((json) => (this.coinsData = json.data.coins));
     },
